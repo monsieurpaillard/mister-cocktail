96dbe63c71c95105fde33379bddb4fab1eecea5f
Romain Paillard <romain.paillard@gmail.com>
2015-11-24 17:43:53 +0100

    validation couple cocktail ingredient ok

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,5 +2,7 @@ class Dose < ActiveRecord::Base
   belongs_to :ingredient
   belongs_to :cocktail
   validates :description, :cocktail, :ingredient, presence: true
-  validates :ingredient, :cocktail, uniqueness: true
+  validates :cocktail, :presence => true
+  validates :ingredient, :presence => true, :uniqueness => {:scope => :cocktail}
+
 end
